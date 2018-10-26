028b8dc256292fbe6a21986ce131568ec7ba5776
Jason Cavaliere <jason@quarterspot.com>
2018-10-26 10:34:18 -0500

    bad ignore, no cookie for you

---
+++ src/PipelineTest.Tests/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PipelineTest.Tests
+{
+    public class Class1
+    {
+
+        [Fact(Skip = "Ignore for demo")]
+        public void should_fail()
+        {
+            Assert.True(false);
+        }
+
+        
+        [Fact]
+        public void should_succeed()
+        {
+            Assert.True(true);
+        }
+
+    }
+}
